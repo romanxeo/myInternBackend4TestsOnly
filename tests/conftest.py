e47ce26d0da5586114b07027e13ebedb3cca9534
romanxeo <romanxeo@gmail.com>
2023-02-24 23:52:18 +0200

    for BE-8

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,13 +8,13 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.pool import NullPool
 from httpx import AsyncClient
 
-#import your app
+# import your app
 from app.main import app
-#import your metadata
+# import your metadata
 from db.models import Base
-#import your test urls for db
+# import your test urls for db
 from core.config import system_config
-#import your get_db func
+# import your get_db func
 from core.connections import get_db
 
 test_db: Database = Database(system_config.db_url_test, force_rollback=True)
@@ -58,3 +58,26 @@ async def prepare_database():
 async def ac() -> AsyncGenerator[AsyncClient, None]:
     async with AsyncClient(app=app, base_url="http://test") as ac:
         yield ac
+
+
+@pytest.fixture(scope='session')
+async def login_user(ac: AsyncClient, users_tokens):
+    async def __send_request(user_email: str, user_password: str):
+        payload = {
+            "user_email": user_email,
+            "user_password": user_password,
+        }
+        response = await ac.post("/auth/login", json=payload)
+        if response.status_code != 200:
+            return response
+        user_token = response.json().get('result').get('access_token')
+        users_tokens[user_email] = user_token
+        return response
+
+    return __send_request
+
+
+@pytest.fixture(scope='session')
+def users_tokens():
+    tokens_store = dict()
+    return tokens_store
---
+++ tests/test_2_company.py
@@ -0,0 +1,171 @@
+from httpx import AsyncClient
+
+
+async def test_create_company_unauthorized(ac: AsyncClient):
+    payload = {
+        "company_name": "company1",
+        "company_description": "string"
+    }
+    response = await ac.post("/company", json=payload)
+    assert response.status_code == 403
+
+
+async def test_bad_create_company__no_name(ac: AsyncClient, users_tokens):
+    headers = {
+        "Authorization": f"Bearer {users_tokens['test1@test.com']}",
+    }
+    payload = {
+        "company_name": "",
+        "company_description": "company_description"
+    }
+    response = await ac.post("/company", json=payload, headers=headers)
+    assert response.status_code == 422
+
+
+async def test_create_company_one(users_tokens, ac: AsyncClient):
+    headers = {
+        "Authorization": f"Bearer {users_tokens['test1@test.com']}",
+    }
+    payload = {
+        "company_name": "test_company_1",
+        "company_description": "company_description_1"
+    }
+    response = await ac.post("/company", json=payload, headers=headers)
+    assert response.status_code == 201
+    assert response.json().get("result").get("company_id") == 1
+
+
+async def test_create_company_two(users_tokens, ac: AsyncClient):
+    headers = {
+        "Authorization": f"Bearer {users_tokens['test1@test.com']}",
+    }
+    payload = {
+        "company_name": "test_company_2",
+    }
+    response = await ac.post("/company", json=payload, headers=headers)
+    assert response.status_code == 201
+    assert response.json().get("result").get("company_id") == 2
+
+
+async def test_create_company_three(users_tokens, ac: AsyncClient):
+    headers = {
+        "Authorization": f"Bearer {users_tokens['test1@test.com']}",
+    }
+    payload = {
+        "company_name": "test_company_3",
+    }
+    response = await ac.post("/company", json=payload, headers=headers)
+    assert response.status_code == 201
+    assert response.json().get("result").get("company_id") == 3
+
+
+async def test_get_all_companies(users_tokens, ac: AsyncClient):
+    headers = {
+        "Authorization": f"Bearer {users_tokens['test1@test.com']}",
+    }
+    response = await ac.get("/companies", headers=headers)
+    assert response.status_code == 200
+    assert len(response.json().get("result").get('companies')) == 3
+
+
+async def test_bad_get_company_by_id_not_found(users_tokens, ac: AsyncClient):
+    headers = {
+        "Authorization": f"Bearer {users_tokens['test1@test.com']}",
+    }
+    response = await ac.get("/company/4", headers=headers)
+    assert response.status_code == 404
+
+
+async def test_get_company_by_id_one(users_tokens, ac: AsyncClient):
+    headers = {
+        "Authorization": f"Bearer {users_tokens['test1@test.com']}",
+    }
+    response = await ac.get("/company/1", headers=headers)
+    assert response.status_code == 200
+    assert response.json().get("result").get("company_id") == 1
+    assert response.json().get("result").get("company_name") == "test_company_1"
+    assert response.json().get("result").get("company_description") == "company_description_1"
+    assert response.json().get("result").get("company_owner_id") == 1
+
+
+async def test_get_company_by_id_tw(users_tokens, ac: AsyncClient):
+    headers = {
+        "Authorization": f"Bearer {users_tokens['test2@test.com']}",
+    }
+    response = await ac.get("/company/2", headers=headers)
+    assert response.status_code == 200
+    assert response.json().get("result").get("company_id") == 2
+    assert response.json().get("result").get("company_name") == "test_company_2"
+    assert response.json().get("result").get("company_description") == None
+    assert response.json().get("result").get("company_owner_id") == 1
+
+
+async def test_bad_update_company__unauthorized(ac: AsyncClient):
+    payload = {
+        "company_name": "company_name_1_NEW",
+        "company_description": "company_description_1_NEW"
+    }
+    response = await ac.put("/company/1", json=payload)
+    assert response.status_code == 403
+
+
+async def test_bad_update_company__not_found(users_tokens, ac: AsyncClient):
+    headers = {
+        "Authorization": f"Bearer {users_tokens['test1@test.com']}",
+    }
+    payload = {
+        "company_name": "company_name_1_NEW",
+        "company_description": "company_description_1_NEW"
+    }
+    response = await ac.put("/company/100", json=payload, headers=headers)
+    assert response.status_code == 404
+
+
+async def test_update_company(users_tokens, ac: AsyncClient):
+    headers = {
+        "Authorization": f"Bearer {users_tokens['test1@test.com']}",
+    }
+    payload = {
+        "company_name": "company_name_1_NEW",
+        "company_description": "company_description_1_NEW"
+    }
+    response = await ac.put("/company/1", json=payload, headers=headers)
+    assert response.status_code == 200
+
+
+async def test_get_company_by_id_one_updated(users_tokens, ac: AsyncClient):
+    headers = {
+        "Authorization": f"Bearer {users_tokens['test1@test.com']}",
+    }
+    response = await ac.get("/company/1", headers=headers)
+    assert response.status_code == 200
+    assert response.json().get("result").get("company_id") == 1
+    assert response.json().get("result").get("company_name") == "company_name_1_NEW"
+    assert response.json().get("result").get("company_description") == "company_description_1_NEW"
+    assert response.json().get("result").get("company_owner_id") == 1
+
+
+async def test_bad_delete_company_one__user_not_owner(users_tokens, ac: AsyncClient):
+    headers = {
+        "Authorization": f"Bearer {users_tokens['test2@test.com']}",
+    }
+    response = await ac.delete("/company/1", headers=headers)
+    assert response.status_code == 403
+
+
+async def test_delete_company_one(users_tokens, ac: AsyncClient):
+    headers = {
+        "Authorization": f"Bearer {users_tokens['test1@test.com']}",
+    }
+    response = await ac.delete("/company/1", headers=headers)
+    assert response.status_code == 200
+
+
+async def test_get_all_companies_after_not_delete(users_tokens, ac: AsyncClient):
+    headers = {
+        "Authorization": f"Bearer {users_tokens['test1@test.com']}",
+    }
+    response = await ac.get("/companies", headers=headers)
+    print(response.json())
+    assert response.status_code == 200
+    assert len(response.json().get("result").get('companies')) == 2
